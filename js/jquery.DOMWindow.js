0e9540cc22f656085a1c51d0f82576388a7ee481
John Hoogstrate <johnhoogstrate@gmail.com>
2012-01-22 21:04:54 +0100

    removed unused code from file

--- js/jquery.DOMWindow.js
+++ js/jquery.DOMWindow.js
@@ -1,36 +1,24 @@
 (function($){
 	
-	//closeDOMWindow
 	$.fn.closeDOMWindow = function(settings){
 		
 		if(!settings){settings={};}
 		
 		var run = function(passingThis){
-			
-			if(settings.anchoredClassName){
-				var $anchorClassName = $('.'+settings.anchoredClassName);
-				$anchorClassName.fadeOut('fast',function(){
-					if($.fn.draggable){
-						$anchorClassName.draggable('destory').trigger("unload").remove();	
-					}else{
-						$anchorClassName.trigger("unload").remove();
-					}
-				});
-				if(settings.functionCallOnClose){settings.functionCallAfterClose();}
-			}else{
-				var $DOMWindowOverlay = $('#DOMWindowOverlay');
-				var $DOMWindow = $('#DOMWindow');
+			        
+            var $DOMWindowOverlay = $('#DOMWindowOverlay');
+            var $DOMWindow = $('#DOMWindow');
 
-				$DOMWindowOverlay.trigger('unload').unbind().remove();																	  
-                $DOMWindow.trigger("unload").remove();
-                $DOMWindow.empty();
-			
-				$(window).unbind('scroll.DOMWindow');
-				$(window).unbind('resize.DOMWindow');
+            $DOMWindowOverlay.trigger('unload').unbind().remove();																	  
+            $DOMWindow.trigger("unload").remove();
+            $DOMWindow.empty();
+        
+            $(window).unbind('scroll.DOMWindow');
+            $(window).unbind('resize.DOMWindow');
+            
+            if($.fn.openDOMWindow.isIE6){$('#DOMWindowIE6FixIframe').remove();}
+            if(settings.functionCallOnClose){settings.functionCallAfterClose();}
 				
-				if($.fn.openDOMWindow.isIE6){$('#DOMWindowIE6FixIframe').remove();}
-				if(settings.functionCallOnClose){settings.functionCallAfterClose();}
-			}	
 		};
 		
 		if(settings.eventType){//if used with $().
@@ -40,10 +28,9 @@
 					return false;
 				});
 			});
-		}else{//else called as $.function
+		}else{
 			run();
 		}
-		
 	};
 	
 	//allow for public call, pass settings
@@ -54,21 +41,16 @@
 		
 		var shortcut =  $.fn.openDOMWindow;
 	
-		//default settings combined with callerSettings////////////////////////////////////////////////////////////////////////
-		
+		//default settings combined with callerSettings
 		shortcut.defaultsSettings = {
 			borderColor:'#ddd',
-			borderSize:'2',
+			borderSize: 2,
 			draggable:0,
 			eventType:null, //click, blur, change, dblclick, error, focus, load, mousedown, mouseout, mouseup etc...
 			fixedWindowY:100,
 			functionCallOnOpen:null,
 			functionCallOnClose:null,
 			height:340,
-			loader:0,
-			loaderHeight:0,
-			loaderImagePath:'',
-			loaderWidth:0,
 			modal:0,
 			overlay:1,
 			overlayColor:'#333',
@@ -78,21 +60,19 @@
 			positionType:'centered', // centered, anchored, absolute, fixed
 			width:460, 
 			windowBGColor: 'rgba(40, 40, 40, 0.85)',
-			windowBGImage:null, // http path
+			windowBGImage:null,
 		};
 		
 		var settings = $.extend({}, $.fn.openDOMWindow.defaultsSettings , instanceSettings || {});
 		
 		//Public functions
-		
 		shortcut.viewPortHeight = function(){ return self.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;};
 		shortcut.viewPortWidth = function(){ return self.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;};
 		shortcut.scrollOffsetHeight = function(){ return self.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;};
 		shortcut.scrollOffsetWidth = function(){ return self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft;};
 		shortcut.isIE6 = typeof document.body.style.maxHeight === "undefined";
 		
-		//Private Functions/////////////////////////////////////////////////////////////////////////////////////////////////////////
-		
+		//Private Functions
 		var sizeOverlay = function(){
 			var $DOMWindowOverlay = $('#DOMWindowOverlay');
 			if(shortcut.isIE6){//if IE 6
@@ -120,17 +100,6 @@
 			}
 		};
 		
-		var centerLoader = function() {
-			var $DOMWindowLoader = $('#DOMWindowLoader');
-			if(shortcut.isIE6){//if IE 6
-				$DOMWindowLoader.css({'left':Math.round(shortcut.viewPortWidth()/2) + shortcut.scrollOffsetWidth() - Math.round(($DOMWindowLoader.innerWidth())/2),'position':'absolute'});
-				$DOMWindowLoader.css({'top':Math.round(shortcut.viewPortHeight()/2) + shortcut.scrollOffsetHeight() - Math.round(($DOMWindowLoader.innerHeight())/2),'position':'absolute'});
-			}else{
-				$DOMWindowLoader.css({'left':'50%','top':'50%','position':'fixed'});
-			}
-			
-		};
-		
 		var fixedDOMWindow = function(){
 			var $DOMWindow = $('#DOMWindow');
 			$DOMWindow.css('left', settings.positionLeft + shortcut.scrollOffsetWidth());
@@ -139,111 +108,80 @@
 		
 		var showDOMWindow = function(instance){
 			if(arguments[0]){
-				$('.'+instance+' #DOMWindowLoader').remove();
 				$('.'+instance+' #DOMWindowContent').fadeIn('fast',function(){if(settings.functionCallOnOpen){settings.functionCallOnOpen();}});
 				$('.'+instance+ '.closeDOMWindow').click(function(){
 					$.closeDOMWindow();	
 					return false;
 				});
 			}else{
-				$('#DOMWindowLoader').remove();
 				$('#DOMWindow').fadeIn('fast',function(){if(settings.functionCallOnOpen){settings.functionCallOnOpen();}});
 				$('#DOMWindow .closeDOMWindow').click(function(){						
 					$.closeDOMWindow();
 					return false;
 				});
-			}
-			
+			}		
 		};
 		
-		var urlQueryToObject = function(s){
-			  var query = {};
-			  s.replace(/b([^&=]*)=([^&=]*)b/g, function (m, a, d) {
-				if (typeof query[a] != 'undefined') {
-				  query[a] += ',' + d;
-				} else {
-				  query[a] = d;
-				}
-			  });
-			  return query;
-		};
-			
 		var run = function(passingThis){
 			
 			//get values from element clicked, or assume its passed as an option
 			settings.windowSourceID = $(passingThis).attr('href') || settings.windowSourceID;
 			settings.windowSourceURL = $(passingThis).attr(settings.windowSourceAttrURL) || settings.windowSourceURL;
 			settings.windowBGImage = settings.windowBGImage ? 'background-image:url('+settings.windowBGImage+')' : '';
-			var urlOnly, urlQueryObject;
-			
-				//overlay & modal
-				if(settings.overlay){
-					$('body').append('<div id="DOMWindowOverlay" style="z-index:10000;display:none;position:absolute;top:0;left:0;background-color:'+settings.overlayColor+';filter:alpha(opacity='+settings.overlayOpacity+');-moz-opacity: 0.'+settings.overlayOpacity+';opacity: 0.'+settings.overlayOpacity+';"></div>');
-					if(shortcut.isIE6){//if IE 6
-						$('body').append('<iframe id="DOMWindowIE6FixIframe"  src="blank.html"  style="width:100%;height:100%;z-index:9999;position:absolute;top:0;left:0;filter:alpha(opacity=0);"></iframe>');
-						sizeIE6Iframe();
-					}
-					sizeOverlay();
-					var $DOMWindowOverlay = $('#DOMWindowOverlay');
-					$DOMWindowOverlay.fadeIn('fast');
-					if(!settings.modal){$DOMWindowOverlay.click(function(){$.closeDOMWindow();});}
-				}
-				
-				//loader
-				if(settings.loader && settings.loaderImagePath !== ''){
-					$('body').append('<div id="DOMWindowLoader" style="z-index:10002;width:'+settings.loaderWidth+'px;height:'+settings.loaderHeight+'px;"><img src="'+settings.loaderImagePath+'" /></div>');
-					centerLoader();
-				}
 
-				//add DOMwindow
-				$('body').append('<div id="DOMWindow" style="background-repeat:no-repeat;'+settings.windowBGImage+';overflow:auto;display:none;height:'+settings.height+'px;width:'+settings.width+'px;background-color:'+settings.windowBGColor+';border:'+settings.borderSize+'px solid '+settings.borderColor+'; position:absolute;z-index:10001"></div>');
-				
-				var $DOMWindow = $('#DOMWindow');
-				//centered, absolute, or fixed
-				switch(settings.positionType){
-					case 'centered':
-						centerDOMWindow();
-						if(settings.height + 50 > shortcut.viewPortHeight()){//added 50 to be safe
-							$DOMWindow.css('top', (settings.fixedWindowY + shortcut.scrollOffsetHeight()) + 'px');
-						}
-					break;
-					case 'absolute':
-						$DOMWindow.css({'top':(settings.positionTop+shortcut.scrollOffsetHeight())+'px','left':(settings.positionLeft+shortcut.scrollOffsetWidth())+'px'});
-						if($.fn.draggable){
-							if(settings.draggable){$DOMWindow.draggable({cursor:'move'});}
-						}
-					break;
-					case 'fixed':
-						fixedDOMWindow();
-					break;
-					case 'anchoredSingleWindow':
-						var anchoredPositions = $(settings.anchoredSelector).position();
-						var anchoredPositionX = anchoredPositions.left + settings.positionLeft;
-						var anchoredPositionY = anchoredPositions.top + settings.positionTop;
-						$DOMWindow.css({'top':anchoredPositionY + 'px','left':anchoredPositionX+'px'});
-								
-					break;
-				}
-				
-				$(window).bind('scroll.DOMWindow',function(){
-					if(settings.overlay){sizeOverlay();}
-					if(shortcut.isIE6){sizeIE6Iframe();}
-					if(settings.positionType == 'centered'){centerDOMWindow();}
-					if(settings.positionType == 'fixed'){fixedDOMWindow();}
-				});
+            //overlay & modal
+            if(settings.overlay){
+                $('body').append('<div id="DOMWindowOverlay" style="z-index:10000;display:none;position:absolute;top:0;left:0;background-color:'+settings.overlayColor+';filter:alpha(opacity='+settings.overlayOpacity+');-moz-opacity: 0.'+settings.overlayOpacity+';opacity: 0.'+settings.overlayOpacity+';"></div>');
+                if(shortcut.isIE6){//if IE 6
+                    $('body').append('<iframe id="DOMWindowIE6FixIframe"  src="blank.html"  style="width:100%;height:100%;z-index:9999;position:absolute;top:0;left:0;filter:alpha(opacity=0);"></iframe>');
+                    sizeIE6Iframe();
+                }
+                sizeOverlay();
+                var $DOMWindowOverlay = $('#DOMWindowOverlay');
+                $DOMWindowOverlay.fadeIn('fast');
+                if(!settings.modal){$DOMWindowOverlay.click(function(){$.closeDOMWindow();});}
+            }
 
-				$(window).bind('resize.DOMWindow',function(){
-					if(shortcut.isIE6){sizeIE6Iframe();}
-					if(settings.overlay){sizeOverlay();}
-					if(settings.positionType == 'centered'){centerDOMWindow();}
-				});
-				
-				$DOMWindow.html(settings.html);
-				
-				showDOMWindow();
-		
-			
-		};//end run()
+            //add DOMwindow
+            $('body').append('<div id="DOMWindow" style="background-repeat:no-repeat;'+settings.windowBGImage+';overflow:auto;display:none;height:'+settings.height+'px;width:'+settings.width+'px;background-color:'+settings.windowBGColor+';border:'+settings.borderSize+'px solid '+settings.borderColor+'; position:absolute;z-index:10001"></div>');
+            
+            var $DOMWindow = $('#DOMWindow');
+            //centered, absolute, or fixed
+            switch(settings.positionType){
+                case 'centered':
+                    centerDOMWindow();
+                    if(settings.height + 50 > shortcut.viewPortHeight()){//added 50 to be safe
+                        $DOMWindow.css('top', (settings.fixedWindowY + shortcut.scrollOffsetHeight()) + 'px');
+                    }
+                break;
+                case 'absolute':
+                    $DOMWindow.css({'top':(settings.positionTop+shortcut.scrollOffsetHeight())+'px','left':(settings.positionLeft+shortcut.scrollOffsetWidth())+'px'});
+                    if($.fn.draggable){
+                        if(settings.draggable){$DOMWindow.draggable({cursor:'move'});}
+                    }
+                break;
+                case 'fixed':
+                    fixedDOMWindow();
+                break;
+            }
+            
+            $(window).bind('scroll.DOMWindow',function(){
+                if(settings.overlay){sizeOverlay();}
+                if(shortcut.isIE6){sizeIE6Iframe();}
+                if(settings.positionType == 'centered'){centerDOMWindow();}
+                if(settings.positionType == 'fixed'){fixedDOMWindow();}
+            });
+
+            $(window).bind('resize.DOMWindow',function(){
+                if(shortcut.isIE6){sizeIE6Iframe();}
+                if(settings.overlay){sizeOverlay();}
+                if(settings.positionType == 'centered'){centerDOMWindow();}
+            });
+            
+            $DOMWindow.html(settings.html);
+            
+            showDOMWindow();    
+		};
 		
 		if(settings.eventType){//if used with $().
 			return this.each(function(index){				  
